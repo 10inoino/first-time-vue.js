3596a875be48711a2891468c41c0df6a03db8942
Hikaru Inomata <inomatahi@icloud.com>
2021-01-11 18:37:37 +0900

    chapter7終了

---
+++ my-cli/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <p>こんにちは！</p>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+p {
+  border: 1px solid red;
+  background-color: yellow;
+}
+</style>
