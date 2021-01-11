ae54883c3c9355868d0e690a02549cccf268f885
Hikaru Inomata <inomatahi@icloud.com>
2021-01-11 22:16:34 +0900

    chapter8終了

--- my-cli/src/App.vue
+++ my-cli/src/App.vue
@@ -1,32 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <p>こんにちは！</p>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/article/108">記事：No.108</router-link>
   </div>
+  <router-view/>
 </template>
 
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  // components: {
-  //   HelloWorld
-  // }
-}
-</script>
-
-<style scoped>
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
-p {
-  border: 1px solid red;
-  background-color: yellow;
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ my-cli/src/components/Article.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div>記事コード：{{ aid }}</div>
+        <div>{{ $route.name }}</div>
+        <div>{{ $route.fullPath }}</div>
+        <div>{{ $route.query }}</div>
+        <div>{{ $route.hash }}</div>
+        <div>{{ $route.matched }}</div>
+        <div>{{ $route.redirectFrom }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Article',
+    props: {
+        aid: String
+    }
+}
+</script>
